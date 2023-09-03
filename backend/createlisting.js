013c93fe6f32e67f106f206bc4c7b858334e8460
allenle123 <ale21100@gmail.com>
2023-09-03 00:31:30 -0700

    updated to reflect fields from user input

--- backend/createlisting.js
+++ backend/createlisting.js
@@ -5,62 +5,106 @@ const AWS = require('aws-sdk');
 const util = require('./utils');
 
 const dynamodb = new AWS.DynamoDB.DocumentClient();
-const itemTable = '491-listings';
+const listingsTable = '491-listings';
 
-async function createListing(itemInfo){
-    const itemName = itemInfo.name;
-    const itemPrice = itemInfo.price;
+async function createlisting(itemInfo){
+    const itemName = itemInfo.itemName
+    const itemDescription = itemInfo.itemDescription
+    const condition = itemInfo.condition
+    const catagory = itemInfo.catagory
+    const price = itemInfo.price
+    
+    // if (!username || !email || !password) {
+    //     return util.buildResponse(401, {
+    //         message: 'All fields are required'
+    //     });
+    // }
+    
+    
+    // const dynamoUser = await getUser(username);
+    // if (dynamoUser && dynamoUser.username){
+    //     return util.buildResponse(403, {message: 'username already exists'})
+        
+    // }
+    
+    // const dynamoEmail = await getEmail(email);
+    // if (dynamoEmail) {
+    //     return util.buildResponse(403, { message: 'Email already exists' });
+    // }
+    
+    // const encryptedPW = bcrypt.hashSync(password.trim(),10);
+
+    const item = {
+        itemName: itemName,
+        itemDescription: itemDescription,
+        condition: condition,
+        catagory: catagory,
+        price: price
+    }
+    
+    const params = {
+        TableName: listingsTable,
+        Item: item
+    }
+    
+    // try {
+    // await dynamodb.put(params).promise();
+    // return { body: 'Successfully created item!' }
+    // } catch (err) {}
+    
+    const savedUserResponse = await saveItem(item);
+    if(!savedUserResponse){
+        return util.buildResponse(503, {message: 'Server error'});
+    }
+    const response = {
+        item: itemInfo
+    }
+    return util.buildResponse(200, response);
 }
 
-const params = {
-    TableName: '491-listings',
-    Item: {
-      // Define your item attributes here
-      // Example: 'attributeName': { dataType: attributeValue }
-      'id': { S: '1' },
-      'itemName': { S: 'Shoes' },
-      'itemPrice': { N: '40' }
-    }
-  };
-  
-  dynamodb.putItem(params, (err, data) => {
-    if (err) {
-      console.error('Error inserting item:', err);
-    } else {
-      console.log('Item inserted successfully:', data);
-    }
-  });
-
-// // Used in below code
-// const item = {
-//     itemName: itemName,
-//     itemPrice: price,
-// }
-
-// const params = {
-//     TableName: listings,
-//     Item: item
-// }
-
-// try {
-//     await dynamodb.put(params).promise();
-//     return { body: 'Successfully created item!' }
-//     } catch (err) {}
-
-// const savedItemResponse = await saveItem(item);
-// if(!savedItemResponse){
-//     return util.buildResponse(503, {message: 'Server error'});
-//     }
-
-
-// async function saveItem(item){
+// async function getUser(username){
 //     const params = {
-//         TableName: itemTable,
-//         Item: item
+//         TableName: userTable,
+//         Key: {
+//             username: username
+//         }
 //     }
-//     return await dynamodb.put(params).promise().then(() => {
-//         return true;
+//     return await dynamodb.get(params).promise().then(response => {
+//         return response.Item;
 //     }, error =>{
-//         console.error('There is an error creating listing: ', error);
+//         console.error('There is an error getting user: ', error);
 //     })
 // }
+
+// async function getEmail(email) {
+//     const params = {
+//         TableName: userTable,
+//         IndexName: 'email-index',
+//         KeyConditionExpression: 'email = :email',
+//         ExpressionAttributeValues: {
+//             ':email': email
+//         }
+//     };
+
+//     const response = await dynamodb.query(params).promise();
+//     if (response.Items.length > 0) {
+//         return response.Items[0]; // Email exists, return the user
+//     } else {
+//         return null; // Email doesn't exist
+//     }
+// }
+
+
+async function saveItem(item){
+    const params = {
+        TableName: listingsTable,
+        Item: item
+    }
+    return await dynamodb.put(params).promise().then(() => {
+        return true;
+    }, error =>{
+        console.error('There is an error saving user: ', error);
+    })
+}
+
+module.exports.createlisting = createlisting;
