7f835384936c350fe9b253f715134d28f3cb3efb
allenle123 <ale21100@gmail.com>
2023-09-12 19:35:27 -0700

    added uuid and more functionality

--- backend/createlisting.js
+++ backend/createlisting.js
@@ -5,52 +5,46 @@ const AWS = require('aws-sdk');
 const util = require('./utils');
 
 const dynamodb = new AWS.DynamoDB.DocumentClient();
-const listingsTable = '491-listings';
+const listingsTable = '491_listings';
 
-async function createListing(itemInfo){
-    const itemName = itemInfo.itemName
-    const itemDescription = itemInfo.itemDescription
-    const condition = itemInfo.condition
-    const catagory = itemInfo.catagory
-    const price = itemInfo.price
+// const { v4: uuidv4 } = require('uuid');
+
+// // Generate a UUID
+// const uuid = uuidv4();
+
+async function createlisting(itemInfo){
+    const uuid = itemInfo.uuid;
+    const itemName = itemInfo.itemName;
+    const itemDescription = itemInfo.itemDescription;
+    const condition = itemInfo.condition;
+    const catagory = itemInfo.catagory;
+    const price = itemInfo.price;
     
-    // if (!username || !email || !password) {
-    //     return util.buildResponse(401, {
-    //         message: 'All fields are required'
-    //     });
-    // }
+    if (!itemName || !itemDescription || !condition|| !catagory || !price) {
+        return util.buildResponse(401, {
+            message: 'All item fields are required'
+        });
+    }
     
-    
-    // const dynamoUser = await getUser(username);
-    // if (dynamoUser && dynamoUser.username){
-    //     return util.buildResponse(403, {message: 'username already exists'})
+    const dynamoPrice = await getPrice(price);
+    if (dynamoPrice < 0){
+        return util.buildResponse(403, {message: 'item price cannot be negative'})
         
-    // }
-    
-    // const dynamoEmail = await getEmail(email);
-    // if (dynamoEmail) {
-    //     return util.buildResponse(403, { message: 'Email already exists' });
-    // }
-    
-    // const encryptedPW = bcrypt.hashSync(password.trim(),10);
+    }
 
     const item = {
+        uuid: uuid,
         itemName: itemName,
         itemDescription: itemDescription,
         condition: condition,
         catagory: catagory,
         price: price
-    }
+    };
     
     const params = {
         TableName: listingsTable,
         Item: item
-    }
-    
-    // try {
-    // await dynamodb.put(params).promise();
-    // return { body: 'Successfully created item!' }
-    // } catch (err) {}
+    };
     
     const savedUserResponse = await saveItem(item);
     if(!savedUserResponse){
@@ -62,19 +56,19 @@ async function createListing(itemInfo){
     return util.buildResponse(200, response);
 }
 
-// async function getUser(username){
-//     const params = {
-//         TableName: userTable,
-//         Key: {
-//             username: username
-//         }
-//     }
-//     return await dynamodb.get(params).promise().then(response => {
-//         return response.Item;
-//     }, error =>{
-//         console.error('There is an error getting user: ', error);
-//     })
-// }
+async function getPrice(price){
+    const params = {
+        TableName: listingsTable,
+        Key: {
+            price: price
+        }
+    }
+    return await dynamodb.get(params).promise().then(response => {
+        return response.Item;
+    }, error =>{
+        console.error('There is an error getting item price: ', error);
+    })
+}
 
 // async function getEmail(email) {
 //     const params = {
@@ -99,12 +93,12 @@ async function saveItem(item){
     const params = {
         TableName: listingsTable,
         Item: item
-    }
+    };
     return await dynamodb.put(params).promise().then(() => {
         return true;
     }, error =>{
-        console.error('There is an error saving item: ', error);
-    })
+        console.error('There is an error saving user: ', error);
+    });
 }
 
-module.exports.createlisting = createlisting;
+module.exports.createListing = createlisting;
--- backend/index.js
+++ backend/index.js
@@ -1,14 +1,16 @@
 const AWS = require('aws-sdk');
-const dynamodb = new AWS.DynamoDB.DocumentClient();
+
 const registerService = require('./register');
 const loginService = require('./login');
 const createListingService = require('./createlisting')
-//const verifyService = require('./verify');
+const profileService = require('./profile')
+const verifyService = require('./verify');
 const util = require('./utils');
 
 const registerPath = '/register';
 const healthPath = '/health';
 const loginPath = '/login';
+const profilePath = '/profile';
 const verifyPath = '/verify';
 const createListing = '/createlisting';
 exports.handler = async(event) => {
@@ -26,12 +28,23 @@ exports.handler = async(event) => {
             const loginBody = JSON.parse(event.body);
             response = loginService.login(loginBody);
             break;
+        case event.httpMethod === 'POST' && event.path === profilePath:
+            const profileBody = JSON.parse(event.body);
+            // response = util.buildResponse(200);
+            response = profileService.edit_profile(profileBody);
+            break;
+        case event.httpMethod === 'GET' && event.path === profilePath:
+            const getProfileBody = JSON.parse(event.body);
+            // response = util.buildResponse(200);
+            response = profileService.get_profile();
+            break;
         case event.httpMethod === 'POST' && event.path === verifyPath:
             const verifyBody = JSON.parse(event.body);
             response = util.buildResponse(200);
             break;
         case event.httpMethod === 'POST' && event.path === createListing:
             const createListingBody = JSON.parse(event.body);
+            // response = util.buildResponse(200);
             response = await createListingService.createListing(createListingBody);
             break;
         default:
