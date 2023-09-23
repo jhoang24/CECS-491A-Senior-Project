afb2a697a803d6ebdbe92232dab404b1de5c4eec
joe-schmoe4 <daedricgun@gmail.com>
2023-09-23 16:03:15 -0700

    Added incomplete post request for profile info

--- 491A/src/app/protected/header/header.component.ts
+++ 491A/src/app/protected/header/header.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from 'src/app/public/services/auth-service/auth.service';
+import { HttpClient } from '@angular/common/http';
+
+
 
 @Component({
   selector: 'app-header',
@@ -7,9 +11,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
+  constructor(private http: HttpClient) { 
+
+  }
 
   ngOnInit(): void {
+    this.getProfileInfo();
+    console.log(this.profileData)
+  }
+
+  private profileData: any = []
+
+ 
+  getProfileInfo(){
+    this.http.post("https://gdl0m2hqx0.execute-api.us-east-1.amazonaws.com/dev/get-profile",{"username":"cone"})
+    .subscribe((res)=>{
+       this.profileData = res;
+  
+    })
+    
   }
 
 }
