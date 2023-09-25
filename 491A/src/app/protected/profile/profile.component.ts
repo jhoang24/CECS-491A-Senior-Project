8519122c5f288c0226475c4dbaa27007f85f14e3
maximillian00 <max.gutierrez13@gmail.com>
2023-09-25 13:16:09 -0700

    Upload profile pic/Change name
    not connected with backend, only works locally for now

--- 491A/src/app/protected/profile/profile.component.ts
+++ 491A/src/app/protected/profile/profile.component.ts
@@ -1,5 +1,5 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
-
 
 @Component({
   selector: 'app-profile',
@@ -7,10 +7,43 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./profile.component.scss']
 })
 export class ProfileComponent implements OnInit {
+  //user: any;
+  name: string = '';
+  bio: string = '';
+  email: string = '';
+  phone: string = '';
+  profilePicture: string | ArrayBuffer | null = null; // Hold the selected image
+  onFileSelected(event: any) {
+    const file: File = event.target.files[0];
 
-  constructor() { }
+    if (file) {
+      const reader = new FileReader();
+
+      reader.onload = (e: any) => {
+        this.profilePicture = e.target.result;
+      };
+
+      reader.readAsDataURL(file);
+    }
+  }
+
+  constructor(private http: HttpClient) {}
 
   ngOnInit(): void {
+   //default data for testing
+   this.name = 'John Doe';
+    this.bio = 'A passionate user of OfferUp.';
+    this.email = 'john@example.com';
+    this.phone = '123-456-7890';
+  }
+
+  saveProfile(){
+    console.log('Profile changes saved ',{ 
+      name: this.name, 
+      bio: this.bio, 
+      email: this.email, 
+      phone: this.phone,
+      profilePicture: this.profilePicture,});
   }
 
 }
