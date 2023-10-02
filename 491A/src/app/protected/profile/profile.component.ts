8614cf0d6898f3e43cca5f847018f1671a144414
joe-schmoe4 <daedricgun@gmail.com>
2023-10-01 17:26:16 -0700

    Removed duplicate profile-setting code

--- 491A/src/app/protected/profile/profile.component.ts
+++ 491A/src/app/protected/profile/profile.component.ts
@@ -1,7 +1,4 @@
-import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
-import { LOCALSTORAGE_TOKEN_KEY } from 'src/app/app.module';
-import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-profile',
@@ -10,40 +7,11 @@ import { Router } from '@angular/router';
 })
 export class ProfileComponent implements OnInit {
   
-  name: string = '';
-  profilePicture: string | ArrayBuffer | null = null; 
-
-  onFileSelected(event: any) {
-    const file: File = event.target.files[0];
-
-    if (file) {
-      const reader = new FileReader();
-
-      reader.onload = (e: any) => {
-        this.profilePicture = e.target.result;
-      };
-
-      reader.readAsDataURL(file);
-    }
-  }
-
-  constructor(private router:Router) {}
+  constructor() {}
 
   ngOnInit(): void {
-   //default data for testing
-   this.name = 'Tony Hawk';
+
   }
 
-  saveProfile(){
-    console.log('Profile changes saved ',{ 
-      name: this.name, 
-      profilePicture: this.profilePicture,});
-  }
-
-  logout() {
-    // Removes the jwt token from the local storage, so the user gets logged out & then navigate back to the "public" routes
-    localStorage.removeItem(LOCALSTORAGE_TOKEN_KEY);
-    this.router.navigate(['../../']);
-  }
 
 }
