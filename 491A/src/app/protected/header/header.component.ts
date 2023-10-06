321bdeb49dc1001f6f1f6dd1386fe6bb9ef8dd52
joe-schmoe4 <daedricgun@gmail.com>
2023-10-06 15:40:18 -0700

    Updated profile service and refactored header

--- 491A/src/app/protected/header/header.component.ts
+++ 491A/src/app/protected/header/header.component.ts
@@ -1,47 +1,29 @@
 import { Component, OnChanges, OnInit, SimpleChanges } from '@angular/core';
-import { AuthService } from 'src/app/public/services/auth-service/auth.service';
-import { HttpClient } from '@angular/common/http';
-import { BehaviorSubject, map, Observable, of, switchMap, tap } from 'rxjs';
 import { LOCALSTORAGE_TOKEN_KEY } from 'src/app/app.module';
 import { Router } from '@angular/router';
 import { GetProfileResponse } from 'src/app/public/interfaces';
-import { profile } from 'console';
+import { ProfileService } from '../services/profile.service';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
-export class HeaderComponent implements OnInit {
-  profileData: any = {
-    user: "",
-    picture: ""
-  }
-  
-  constructor(private http: HttpClient, private authservice: AuthService, private router:Router) { 
+export class HeaderComponent implements OnInit{
+
+  constructor(private router:Router, private profileService: ProfileService) { 
   }
 
   ngOnInit(): void {
-    this.getProfileInfo();
-
+    this.profileService.getProfileInfo();
   }
 
   // Since profileData's fields are initialized as empty, this get function will update this.profileData.picture after the http post request
-  get profilePicture(): string{
-    return "data:image/png;base64," + this.profileData.picture;
+  get picture() {
+    return "data:image/png;base64," + this.profileService.profilePicture;
   }
-
-  getProfileInfo(): void{
-    this.http.post("https://gdl0m2hqx0.execute-api.us-east-1.amazonaws.com/dev/get-profile",{"username":"cone"})
-
-    .subscribe({
-      next: (res) => {
-        this.profileData=res;
-      
-      }
-    })
-  }
-
+  
+  
   logout(): void{
     // Removes the jwt token from the local storage, so the user gets logged out & then navigate back to the "public" routes
     localStorage.removeItem(LOCALSTORAGE_TOKEN_KEY);
--- 491A/src/app/protected/services/profile.service.ts
+++ 491A/src/app/protected/services/profile.service.ts
@@ -1,9 +1,33 @@
-import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Injectable, OnInit } from '@angular/core';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProfileService {
+  profileData: any = {
+    user: "",
+    picture: ""
+  }
+  
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  // Since profileData's fields are initialized as empty, this get function will update this.profileData.picture after the http post request
+  get profilePicture(): string {
+    return this.profileData.picture;
+   
+  }
+
+  getProfileInfo(): void{
+    this.http.post("https://gdl0m2hqx0.execute-api.us-east-1.amazonaws.com/dev/get-profile",{"username":"cone"})
+
+    .subscribe({
+      next: (res) => {
+        this.profileData=res;
+      
+      }
+    })
+  }
+  
 }
