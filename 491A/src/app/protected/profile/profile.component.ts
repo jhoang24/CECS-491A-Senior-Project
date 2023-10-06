d3a5223de87d5b6857f7d034849a47024cf1019e
joe-schmoe4 <daedricgun@gmail.com>
2023-10-06 15:45:14 -0700

    Updated profile component to use profileservice for profile picture retrieval

--- 491A/src/app/protected/profile/profile.component.ts
+++ 491A/src/app/protected/profile/profile.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ProfileService } from '../services/profile.service';
 
 @Component({
   selector: 'app-profile',
@@ -7,11 +8,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProfileComponent implements OnInit {
   
-  constructor() {}
+  constructor(private profileService: ProfileService) {}
 
   ngOnInit(): void {
-
+    this.profileService.getProfileInfo();
   }
+
+  get picture() {
+    return "data:image/png;base64," + this.profileService.profilePicture;
+  }
+  
 
 
 }
