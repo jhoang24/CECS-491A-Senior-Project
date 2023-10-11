644dc9dd8cca82286e6253836c0b89857c4b7dd2
joe-schmoe4 <daedricgun@gmail.com>
2023-10-10 20:30:07 -0700

    Categories bar is fully functional fix

--- 491A/src/app/protected/header/header.component.ts
+++ 491A/src/app/protected/header/header.component.ts
@@ -4,12 +4,16 @@ import { Router } from '@angular/router';
 import { GetProfileResponse } from 'src/app/public/interfaces';
 import { ProfileService } from '../services/profile.service';
 
+
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent implements OnInit{
+  enteredButton = false;
+  isMatMenuOpen = false;
+
 
   constructor(private router:Router, private profileService: ProfileService) { 
   }
@@ -28,6 +32,39 @@ export class HeaderComponent implements OnInit{
     // Removes the jwt token from the local storage, so the user gets logged out & then navigate back to the "public" routes
     localStorage.removeItem(LOCALSTORAGE_TOKEN_KEY);
     this.router.navigate(['../../']);
+  }
+
+  menuenter() {
+    this.isMatMenuOpen = true;
+  }
+
+  menuLeave(trigger: any) {
+    setTimeout(() => {
+      if (!this.enteredButton) {
+        this.isMatMenuOpen = false;
+        trigger.closeMenu();
+      } else {
+        this.isMatMenuOpen = false;
+      }
+    }, 80)
+  }
+
+  buttonEnter(trigger: any) {
+    setTimeout(() => {
+        trigger.openMenu();
+    })
+  }
+
+  buttonLeave(trigger: any) {
+    setTimeout(() => {
+      if (this.enteredButton && !this.isMatMenuOpen) {
+        trigger.closeMenu();
+      } if (!this.isMatMenuOpen) {
+        trigger.closeMenu();
+      } else {
+        this.enteredButton = false;
+      }
+    }, 100)
   }
     
 }
