671b4e210c92fc9085c108a04d2b43864bec5628
joe-schmoe4 <daedricgun@gmail.com>
2023-10-18 16:10:18 -0700

    Added selling component

---
+++ 491A/src/app/protected/selling/selling.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-selling',
+  templateUrl: './selling.component.html',
+  styleUrls: ['./selling.component.scss']
+})
+export class SellingComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
