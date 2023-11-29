5ef0002f536bd2e54b0b2810ca546ebb38a85f44
joe-schmoe4 <daedricgun@gmail.com>
2023-11-29 15:57:40 -0800

    Redesigned products display and delete-account

--- 491A/src/app/protected/delete-account/delete-account.component.ts
+++ 491A/src/app/protected/delete-account/delete-account.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { UntypedFormControl, UntypedFormGroup, Validators } from '@angular/forms';
+import { CustomValidators } from 'src/app/public/custom-validator';
+import { AuthService } from 'src/app/public/services/auth-service/auth.service';
+import { tap } from 'rxjs';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-delete-account',
@@ -6,10 +11,53 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./delete-account.component.scss']
 })
 export class DeleteAccountComponent implements OnInit {
+  success: string | null = null;
+  error: string | null = null;
 
-  constructor() { }
+  oldPassword: string = '';
+  changeForm = new UntypedFormGroup({
+    oldPassword: new UntypedFormControl(null, Validators.required),
+    password: new UntypedFormControl(null,Validators.required),
+    passwordConfirm: new UntypedFormControl(null, Validators.required)
+  },
+    {validators: CustomValidators.passwordsMatching}
+  )
+
+  constructor(
+    private router: Router,
+    private authService: AuthService
+  ) {}
 
   ngOnInit(): void {
+    console.log(this.authService.checkOldPassword("asd"))
   }
 
-}
+  validateOldPassword(control: UntypedFormControl){
+    const inputOldPassword = control.value;
+    const isOldPasswordValid = this.authService.checkOldPassword(inputOldPassword);
+    return isOldPasswordValid ? null : {invalidOldPassword: true};
+  }
+
+  confirm() {
+    // if (!this.changeForm.valid) {
+    //   return;
+    // }
+    // const enteredOldPassword = this.changeForm.get('oldPassword')?.value;
+    // const newPassword = this.changeForm.get('password')?.value;
+    
+
+    // //console.log('oldPassword:', oldPassword);
+    // const token = this.authService.returnToken();
+    // console.log('Token', token);
+
+    // if (this.authService.checkOldPassword(oldPassword)) {
+    //   // Password matches, proceed with updating the password
+    //   this.authService.updatePassword(newPassword).pipe(
+    //     tap(() => this.router.navigate(['']))
+    //   ).subscribe();
+    // } else {
+    //   // Password doesn't match, handle accordingly (e.g., show error message)
+    //   this.error = 'Invalid email address.'
+    // }
+    }
+  }
